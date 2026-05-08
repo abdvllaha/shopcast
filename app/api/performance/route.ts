96d857b3a8ca95bde1941d00776b17667b293776
Abdullah Abdussamad <abdvllaha@proton.me>
2026-05-07 23:32:31 -0400

    Fix env type assertions

--- app/api/performance/route.ts
+++ app/api/performance/route.ts
@@ -9,8 +9,8 @@ export async function GET(request: Request) {
   }
 
   const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
 
   const { data: logs, error } = await supabase
